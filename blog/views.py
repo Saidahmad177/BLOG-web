684256707e54c133e7f7264ebb766cdb23a8a823
saidahmad177 <saidahmad177@mail.ru>
2023-02-25 16:07:34 +0500

    blog-pjoct

---
+++ blog/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Yangilik
+
+
+# Create your views here.
+class HomePageView(ListView):
+    model = Yangilik
+    template_name = 'home.html'
