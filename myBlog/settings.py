f2a9c585731cfd3c42d63deed1a519d10fc65cbc
saidahmad177 <saidahmad177@mail.ru>
2023-03-01 20:05:22 +0500

    update-template

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -116,7 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATICFILES_DIRS = BASE_DIR / 'static'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
