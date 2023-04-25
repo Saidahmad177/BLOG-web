26189790a80ddead3e6b5c94e2158209c67b3de7
Saidahmad177 <saidahmad177@mail.ru>
2023-04-25 22:47:36 +0500

    deploy

--- myBlog/urls.py
+++ myBlog/urls.py
@@ -1,8 +1,8 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,6 +11,5 @@ urlpatterns = [
     path('', include('blog.urls')),
 
 ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
